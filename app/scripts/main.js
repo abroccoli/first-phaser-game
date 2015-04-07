81837bc985f1a5a8173f6ea164cee7de41c067c1
abroccoli <acbrocco@syr.edu>
2015-04-07 12:55:19 -0400

    added center wall, made character wrap around world

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,53 +6,51 @@ function preload(){
   game.load.spritesheet('hero', 'images/YeOldyNecroGuy.png', 16, 15, 24, 18, 5.8);
   game.load.spritesheet('fire', 'images/rsz_fireballs_transparent.png', 30, 16, -1, 0, 0);
   game.load.image('platform', 'images/winter_ground/ground2.png');
+  game.load.image('wallLeft', 'images/winter_ground/ground4.png');
 }
 
-var sprite1, cursors, platforms, ground, attackkey, fireballs;
+var sprite1, cursors, platforms, ground, attackkey, fireballs, midwallLeft, midwallRight;
 
 function create(){
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  platforms = game.add.group();
   fireballs = game.add.group();
+  game.physics.arcade.enable(fireballs);
 
-  platforms.enableBody = true;
-  fireballs.enableBody = true;
+  World.initialize();
 
-  ground = game.add.tileSprite( 0, game.world.height - 50, game.world.width, 0, 'platform');
-  ground.physicsType = Phaser.SPRITE;
-  platforms.add(ground);
-
-  sprite1 = game.add.sprite(100, 530, 'hero');
-  sprite1.scale.x = 2;
-  sprite1.scale.y = 2;
-
-  game.physics.arcade.enable([sprite1, platforms, ground, fireballs]);
-  ground.body.immovable = true;
-  ground.body.allowGravity = false;
-
-  sprite1.body.gravity.y = 500;
-  sprite1.body.collideWorldBounds = true;
-  sprite1.animations.add('right', [6,8,9], 10, true);
-  sprite1.animations.add('up', [7], 10, true);
-  sprite1.animations.add('down', [9], 10, true);
-  sprite1.animations.add('attack', [12], 10, true);
-  sprite1.flipped = false;
-
-  cursors = game.input.keyboard.createCursorKeys();
-  attackkey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
+  Character.build();
 }
 
 function render(){
 }
 
 function update(){
-  game.physics.arcade.collide(sprite1, platforms);
+  game.world.wrap(sprite1, 0, true, true, false);
+  if(fireballs.length > 0){
+    game.physics.arcade.collide(sprite1, platforms);
+    game.physics.arcade.collide(platforms, fireballs);
+  }else{
+    game.physics.arcade.collide(sprite1, platforms);
+  }
   controls();
+  Fireballs.listen();
 }
 
 var Character = {
+  build: function(){
+    sprite1 = game.add.sprite(100, 530, 'hero');
+    sprite1.scale.x = 2;
+    sprite1.scale.y = 2;
+    game.physics.arcade.enable(sprite1);
+    sprite1.body.gravity.y = 500;
+    sprite1.animations.add('right', [6,8,9], 10, true);
+    sprite1.animations.add('up', [7], 10, true);
+    sprite1.animations.add('down', [9], 10, true);
+    sprite1.animations.add('attack', [12], 10, true);
+    sprite1.flipped = false;
+  },
+
   movement: {
     runRight: function(){
       if (sprite1.flipped === true){
@@ -78,21 +76,72 @@ var Character = {
       sprite1.animations.play('down');
     },
     attack: function(){
-      var fireball;
-      if (sprite1.flipped === true){
-        fireball = fireballs.create(sprite1.body.x + sprite1.body.width / 2 - 20, sprite1.body.y + sprite1.body.height / 2, 'fire');
-        fireball.scale.x *= -1;
-        fireball.body.velocity.x = -400;
-      } else{
-        fireball = fireballs.create(sprite1.body.x + sprite1.body.width / 2 + 10, sprite1.body.y + sprite1.body.height / 2, 'fire');
-        fireball.body.velocity.x = 400;
-      }
-      fireball.outOfBoundsKill = true;
+      Fireballs.create();
     }
   }
 };
 
+var Fireballs = {
+  create: function(){
+    var fireball;
+    if (sprite1.flipped === true){
+      fireball = fireballs.create(sprite1.body.x + sprite1.body.width / 2 - 20, sprite1.body.y + sprite1.body.height / 2, 'fire');
+      fireball.scale.x *= -1;
+      fireball.body.velocity.x = -400;
+    } else{
+      fireball = fireballs.create(sprite1.body.x + sprite1.body.width / 2 + 10, sprite1.body.y + sprite1.body.height / 2, 'fire');
+      fireball.body.velocity.x = 400;
+    }
+  },
+  listen:function(){
+    for(var i=0; i<=fireballs.length-1; i++){
+      fireballs.children[i].checkWorldBounds = true;
+      if (fireballs.children[i].inWorld === false){
+        fireballs.children[i].destroy();
+      }else{
+        if (fireballs.children[i].body.velocity.x === 0){
+          fireballs.children[i].destroy();
+        }
+      }
+    }
+  }
+};
+
+var World = {
+  initialize: function(){
+    platforms = game.add.group();
+
+    platforms.enableBody = true;
+    fireballs.enableBody = true;
+
+    this.build();
+
+    game.physics.arcade.enable([platforms, ground]);
+  },
+  build: function(){
+    ground = game.add.tileSprite( -20, game.world.height - 50, game.world.width + 20, 0, 'platform');
+    platforms.add(ground);
+    ground.body.immovable = true;
+    ground.body.allowGravity = false;
+
+    midwallLeft = game.add.tileSprite(game.world.width/2 - 50, game.world.height-300, 50, game.world.height/2, 'wallLeft');
+    platforms.add(midwallLeft);
+    midwallLeft.body.immovable = true;
+    midwallLeft.body.allowGravity = false;
+
+    midwallRight = game.add.tileSprite(game.world.width/2 + 25, game.world.height-300, 50, game.world.height/2, 'wallLeft');
+    platforms.add(midwallRight);
+    midwallRight.body.immovable = true;
+    midwallRight.body.allowGravity = false;
+    midwallRight.anchor.setTo(0.5,0);
+    midwallRight.scale.x *= -1;
+  }
+};
+
 var controls = function(){
+  cursors = game.input.keyboard.createCursorKeys();
+  attackkey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
   sprite1.anchor.setTo(0.5,0.5);
   sprite1.body.velocity.x = 0;
 
